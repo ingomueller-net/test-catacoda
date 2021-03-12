b89b55269eeb430f394b90a13f5846f667fef6fb
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-03-12 11:26:31 +0100

    xxx-pgcli

--- sql-interactive-part-1/assets/pgcli.sh
+++ sql-interactive-part-1/assets/pgcli.sh
@@ -2,5 +2,5 @@
 
 docker run --rm -it \
     --link postgres-server \
-    -v /root/.config/pgcli/config:/root/.config/pgcli/config \
+    -v /root/.config/pgcli/:/root/.config/pgcli/ \
     ingomuellernet/pgcli "$@"
