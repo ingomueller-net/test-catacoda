6efc74fa4620a3057affb8dddac8141d90724f69
Ingo Mueller <ingo.mueller@inf.ethz.ch>
2021-03-12 10:28:05 +0100

    xxx-populate

--- sql-interactive-part-1/assets/populate.sh
+++ sql-interactive-part-1/assets/populate.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-docker cp $HOME/employee.zip postgres-server:/tmp/employee.zip
-docker exec --workdir /tmp postgres-server unzip employee.zip
+docker cp $HOME/employee.zip postgres-server:/var/lib/postgresql/employee.zip
+docker exec --workdir /var/lib/postgresql postgres-server unzip employee.zip
 
 echo "Create and populate 'employee' database."
 docker exec postgres-server sh -c 'psql -U postgres -c "CREATE DATABASE employee;"'
-docker exec postgres-server sh -c "psql -U postgres employee < /tmp/employee/schema.sql"
-docker exec postgres-server sh -c "psql -U postgres -d employee -f /tmp/employee/inserts.sql"
+docker exec postgres-server sh -c "psql -U postgres employee < /var/lib/postgresql/employee/schema.sql"
+docker exec postgres-server sh -c "psql -U postgres -d employee -f /var/lib/postgresql/employee/inserts.sql"
